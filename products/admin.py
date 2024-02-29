8864092c40d13b302471aa7ef5dfec097cb61d53
semoleg1986 <semoleg1986@gmail.com>
2024-02-29 12:59:03 +0400

    feat: impl. product, lesson

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Lesson
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner', 'start_date', 'cost', 'min_group_size', 'max_group_size']
+    list_filter = ['owner', 'start_date']
+    search_fields = ['name']
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product', 'video_url']
+    list_filter = ['product']
+    search_fields = ['title']
--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_products')
+    name = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    min_group_size = models.IntegerField()
+    max_group_size = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='lessons')
+    title = models.CharField(max_length=255)
+    video_url = models.URLField()
+
+    def __str__(self):
+        return self.title
